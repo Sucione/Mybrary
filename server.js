a2ca965f50431ca34b47b21910a1e891d66ebeed
Sucione <Sucione2004@gmail.com>
2020-06-24 20:59:02 +0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,16 +5,24 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express");
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const path = require("path");
+const viewPath = path.join(__dirname , "./views");
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const bodyParser = require("body-parser");
+
 const indexRouter = require('./routes/index');
-const mongoose = require('mongoose')
-const db = mongoose.connection
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', viewPath);
 app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}));
+
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 db.on('error', error => console.error(error));
